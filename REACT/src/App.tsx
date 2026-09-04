1ae3deaee05637f3c54ac9d37b697c54e73e24d3
XCode0033 <XCode0033@gmail.com>
2026-09-04 11:15:53 -0400

    recommited

--- REACT/src/App.tsx
+++ REACT/src/App.tsx
@@ -1,16 +1,16 @@
 // App.tsx
 import './App.css'
 import HomePage from './Pages/HomePage.tsx'
-import AddExpensePage from './Pages/AddExpensePage.tsx'
 import {Route, Routes} from 'react-router'
 import Footer from './Components/Footer.tsx'
+// import AddTransactionsPage from './Pages/AddTransactionsPage.tsx'
 
 function App() {
   return (
   <main className='page-content pb-20'>
     <Routes>
       <Route path='/' element={<HomePage />} />
-      <Route path='/add-expense' element={<AddExpensePage />} />
+      
     </Routes>
 
     <div className="fixed bottom-0 inset-x-0 border-t border-border bg-surface">
